24806e71eb3773cf74bbe05d1137d457e695f039
114135 <u82257241@false>
2016-09-01 09:09:48 +0200

    EInrichtung auf Live Server

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $varTest = file_get_contents('C:/xampp/htdocs/Cloud/web/uploads/test.php');
+        $varTest = file_get_contents('uploads/test.php');
         return $this->render('MainBundle:Default:index.html.twig', array(
             'content' => $varTest,
         ));
